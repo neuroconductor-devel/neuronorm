941df724494fe4a45b6226cd20ad54a4e83e0063
DavidPayares <43214524+DavidPayares@users.noreply.github.com>
2021-02-18 15:41:15 +0100

    Create example.R

---
+++ inst/example.R
@@ -0,0 +1,25 @@
+library('neuronorm')
+library('oro.nifti')
+
+
+# Get general folder
+folder <- system.file("extdata", package = "neuronorm")
+
+# Get covariates
+covariates <- system.file("covariates.txt", package = "neuronorm")
+
+# Read covariates information
+clinical_info <- read.csv(file = covariates, sep = ';')
+
+
+# Preprocess MRI scans: 'N4' inhomogeneity correction, 'SyN' non-linear transformation to MNI152 atlas template
+# Brain extraction, Spatial informed MRI scan , a.k.a., brain segmentation and RAVEL intensity normalization.
+paths_preprocess_patients <- preprocess_patients(folder, clinical_info)
+
+
+# Outputs paths of the preprocessed MRI scans.
+paths_preprocess_patients
+
+# visualize a preprocessed MRI scan for a patient.
+img <- readNIfTI(file.path(paths_preprocess_patients$patient01$ravel))
+orthographic(img)
