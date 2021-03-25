94ffc5214b6919b2142eed22d70a27f89561bb20
DavidPayares <43214524+DavidPayares@users.noreply.github.com>
2021-03-25 16:16:27 +0100

    Update example.R

--- inst/example.R
+++ inst/example.R
@@ -32,3 +32,7 @@ require('oro.nifti')
 img <- readNIfTI(file.path(paths_preprocess_patients$patient04$ravel))
 orthographic(img)
 
+devtools::install('/mnt/d/M.Sc. Gesopatial Tecnologies/Thesis/Resources/Code/neuronorm')
+
+
+remove.packages('neuronorm')
